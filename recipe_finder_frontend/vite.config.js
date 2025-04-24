ab7b6c3a6d3676aa225725851f225231d7219e00
Phong <lenamphong2000@gmail.com>
2025-04-25 02:46:15 +1000

    Hello

--- recipe_finder_frontend/vite.config.js
+++ recipe_finder_frontend/vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-//
-// git add .
-// git add vite.config.js
-// git commit -m "Add vite config.js"
-// git push origin main
-
+// 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
