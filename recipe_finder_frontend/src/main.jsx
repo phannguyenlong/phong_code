905a942230d2194ebeb6f8b756d1b0fcd5618dbd
hieunt1 <hieunt1@vivas.vn>
2025-04-25 00:24:10 +0700

    Add config

--- recipe_finder_frontend/src/main.jsx
+++ recipe_finder_frontend/src/main.jsx
@@ -1,10 +1,10 @@
 // src/main.jsx
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { MantineProvider, createTheme } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
-import '@mantine/core/styles.css';
+import App from './App';
 import './index.css';
 
 const theme = createTheme({
@@ -28,7 +28,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/assignment2">
         <App />
       </BrowserRouter>
     </MantineProvider>
