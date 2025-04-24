ce65af31d0e8e832666ed43efa5b454313bbac4c
hieunt1 <hieunt1@vivas.vn>
2025-04-24 23:40:04 +0700

    add vite config

--- recipe_finder_frontend/vite.config.js
+++ recipe_finder_frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+//
+// git add .
+// git add vite.config.js
+// git commit -m "Add vite config.js"
+// git push origin main
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/assignment2",
   resolve: {
     alias: {
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
