3d5b9c85519772bf56a3c8a4f2b94c2a2c91d228
sheepu0711 <blacksheep07112001@gmail.com>
2025-04-28 22:51:42 +0700

    Fix rate limiter configuration to allow 100 requests per minute per IP

--- recipe_finder_frontend/src/config.js
+++ recipe_finder_frontend/src/config.js
@@ -1,6 +1,3 @@
 
-
-
-
 // Base URL for all API requests
-export const API_BASE_URL = import.meta.env.API;
+export const API_BASE_URL = await import.meta.env.VITE_API_BASE_URL;
--- recipe_finder_frontend/vite.config.js
+++ recipe_finder_frontend/vite.config.js
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/assignment2",
-  server: {
-    allowedHosts: ['n12122882.ifn666.com'],
-  },
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/assignment2",
+//   server: {
+//     allowedHosts: ['n12122882.ifn666.com'],
+//   },
 
-})
+// })
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:3000'}`;
+  const PORT = `${env.VITE_PORT ?? '3000'}`;
+
+  return {
+    plugins: [react()],
+    base: "/assignment2",
+    server: {
+      allowedHosts: ['n12122882.ifn666.com'],
+    },
+  };
+});
